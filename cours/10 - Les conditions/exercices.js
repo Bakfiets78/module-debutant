df46c207689b371f3f704d6c409082a328b9679b
Ounaceur Mustafa <70950844+Bakfiets78@users.noreply.github.com>
2020-10-11 14:06:04 +0200

    Update exercices.js

--- cours/10 - Les conditions/exercices.js
+++ cours/10 - Les conditions/exercices.js
@@ -49,13 +49,16 @@ function exercice0(ageDuClient, poidsDuClient) { // Ne supprimez/commentez pas c
 
   // <===== VOTRE CODE ICI: Insérez/modifiez le code à partir d'ici =====>
   // Utilisez les variables ageDuClient et poidsDuClient pour construire les conditions
-
+  let ageDuClient = 20;
+  let poidsDuClient = 78;
+ 
+  
   // Décommentez le code de Tom ci-dessous, remplacez les "??" par une expression valide
-  // if (??) {
-  //   console.log("Vous pouvez monter dans ce manège");
-  // } else {
-  //   console.log("Vous ne pouvez pas monter dans ce manège");
-  // }
+   if (ageDuClient >= limiteAge && poidsDuClient <= limitePoids) {
+     console.log("Vous pouvez monter dans ce manège");
+   } else {
+     console.log("Vous ne pouvez pas monter dans ce manège");
+   }
 
   // <===== FIN DE VOTRE CODE CI-DESSUS =====>
 
@@ -70,8 +73,11 @@ function exercice1(ageDuClient, poidsDuClient) { // Ne supprimez/commentez pas c
   // Utilisez les variables ageDuClient et poidsDuClient pour construire les conditions
 
   // Décommentez le code de Tom ci-dessous
-  // console.log("Bienvenue dans ce nouveau manège pour les enfants !");
-  // console.log("Désolé mais tu es trop grand pour monter dans ce manège.");
+  if ( ageDuClient <= limiteAgeEnfant || poidsDuClient <= limitePoidsEnfant ){
+   console.log("Bienvenue dans ce nouveau manège pour les enfants !");
+  }else {
+   console.log("Désolé mais tu es trop grand pour monter dans ce manège.");
+  }
 
   // <===== FIN DE VOTRE CODE CI-DESSUS =====>
 
@@ -86,10 +92,19 @@ function exercice2(ageDuClient, poidsDuClient) { // Ne supprimez/commentez pas c
   // Utilisez les variables ageDuClient et poidsDuClient pour construire les conditions
 
   // Décommentez le code de Tom ci-dessous
-  // console.log("Bienvenue dans le manège !");
-  // console.log("Vous ne pouvez pas monter : les sièges ne supporteraient pas votre poids.");
-  // console.log("Vous ne pouvez pas monter : ce manège est réservé aux adultes.");
-  // console.log("Vous devriez vite arrêter de manger des frites !");
+  
+  if ( ageDuClient <= limiteAge &&  poidsDuClient <= limitePoids ){
+   console.log("Bienvenue dans le manège !");
+  }
+  if ( poidsDuClient > limitePoids ) {
+   console.log("Vous ne pouvez pas monter : les sièges ne supporteraient pas votre poids.");
+  }
+  if ( ageDuClient <= limiteAge && ageDuClient <= limiteAgeEnfant ){
+   console.log("Vous ne pouvez pas monter : ce manège est réservé aux adultes.");
+  }
+  if ( poidsDuClient > limitePoids ){
+   console.log("Vous devriez vite arrêter de manger des frites !");
+  }
 
   // <===== FIN DE VOTRE CODE CI-DESSUS =====>
 
@@ -103,12 +118,12 @@ function exercice3() { // Ne supprimez/commentez pas cette ligne
   // <===== VOTRE CODE ICI: Insérez/modifiez le code à partir d'ici =====>
 
   // Décommentez le code de Tom ci-dessous
-  // let zero = 0;
-  // if (zero = 0) {
-  //   console.log("La variable zero vaut bien zéro");
-  // } else {
-  //   console.log("Oops ! Il semble y avoir un bogue !");
-  // }
+   let zero = 0;
+   if (zero === 0) {
+     console.log("La variable zero vaut bien zéro");
+   } else {
+     console.log("Oops ! Il semble y avoir un bogue !");
+   }
 
   // <===== FIN DE VOTRE CODE CI-DESSUS =====>
 
@@ -122,14 +137,14 @@ function exercice4() { // Ne supprimez/commentez pas cette ligne
   // <===== VOTRE CODE ICI: Insérez/modifiez le code à partir d'ici =====>
 
   // Décommentez le code de Tom ci-dessous
-  // let reponse = 42;
-  // let condition = false;
+   let reponse = 42;
+   let condition = false;
+   condition = true;
+   reponse = 1337; 
 
-  // if (condition)
-  //   condition = true;
-  //   reponse = 1337;
-
-  // console.log("La réponse est " + reponse);
+   if (condition){
+     console.log("La réponse est " + reponse);
+   }
 
   // <===== FIN DE VOTRE CODE CI-DESSUS =====>
 
@@ -142,11 +157,15 @@ function exercice5() { // Ne supprimez/commentez pas cette ligne
   // <===== VOTRE CODE ICI: Insérez/modifiez le code à partir d'ici =====>
 
   // Décommentez le code de Tom ci-dessous
-  // let reponse = 1337;
-  // let condition = false;
-
-  // if (condition) reponse = 42; console.log("La réponse est toujours " + reponse);
-  // console.log("Seule cette ligne devrait s'afficher");
+   let reponse = 1337;
+   let condition = false;
+   reponse = 42;
+  
+   if (condition){
+     console.log("La réponse est toujours " + reponse);
+   }else {
+     console.log("Seule cette ligne devrait s'afficher");
+   }
 
   // <===== FIN DE VOTRE CODE CI-DESSUS =====>
 
